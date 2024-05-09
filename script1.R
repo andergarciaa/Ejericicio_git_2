50c3948655ccb3841bd314732b1f845a871db2e6
Ander <ander.garciaa@alumni.mondragon.edu>
2024-05-09 19:02:32 +0200

    Committeo por segunda vez

--- script1.R
+++ script1.R
@@ -1,16 +1,37 @@
+#1.	Inicializa un repositorio remoto sin gitignore ni readme
+#2.	Clona en local
+#3.	En el directorio de trabajo donde hayas inicializado el repositorio genera 
+#   una carpeta de datos y guarda el xlsx
+#4.	Genera un proyecto de R
+
+
+# ::  5.	Carga el fichero y revisa formatos
+
 library(dplyr)
 library(readxl)
 dir()
 df <- read_excel("online_retail_II.xlsx" )
 
 str(df)
-
 df$`Customer ID`<- as.character(df$`Customer ID`)
 
-summary(df)
+# ::  6.	Haz un análisis básico de la información contenida en el fichero
 
-# posibles outliers en Price y Quantity
+   summary(df)    # posibles outliers en Price y Quantity
 
-a <- length(unique(df$Invoice))
-b <- length(df$Invoice)
-# pedidos unicos: [1] 28816
+
+# ::  7.	Calcula cuántos pedidos distintos hay en el fichero
+
+   length(unique(df$Invoice))   # pedidos unicos: [1] 28816
+ 
+   
+# ::  8.	Genera un fichero gitignore para que no sub al repositorio los ficheros .xlsx
+# ::  9.	Comitea por primera vez
+ 
+   
+# ::  10.	Calcula el precio medio de producto por pedido
+precio_medio_pedido <- df %>% 
+  group_by(Invoice) %>% 
+  summarise(precio_medio = mean(Price))
+precio_medio_pedido
+
